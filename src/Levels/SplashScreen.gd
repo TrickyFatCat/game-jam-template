2557b2c9d4a6cd1123bc474e0ec708a6712de5dd
Tricky Fat Cat <tricky.fat.cat@gmail.com>
2021-02-06 23:06:51 +0300

    Refactor timer creation code

--- src/Levels/SplashScreen.gd
+++ src/Levels/SplashScreen.gd
@@ -10,10 +10,8 @@ var _splash_timer : Timer
 func _on_SplashTimer_timeout() -> void:
 	Events.emit_signal("load_level", {"target_level": next_level})
 
+
 func init() -> void:
-	_splash_timer = Timer.new()
-	self.add_child(_splash_timer)
-	_splash_timer.name = "SplashTimer"
-	_splash_timer.wait_time = SPLASH_DURATION
+	_splash_timer = Utility.create_new_timer(self, SPLASH_DURATION, true)
 	_splash_timer.connect("timeout", self, "_on_SplashTimer_timeout")
 	Events.connect("transition_screen_opened", _splash_timer, "start")
