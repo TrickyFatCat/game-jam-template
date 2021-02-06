a3069cf93c7f01ba459e61605616b0c19bb3b078
Tricky Fat Cat <tricky.fat.cat@gmail.com>
2021-02-06 22:03:47 +0300

    Update base level logic

--- src/Levels/BaseLevel.gd
+++ src/Levels/BaseLevel.gd
@@ -3,3 +3,22 @@
 class_name BaseLevel
 extends Node
 
+const LOAD_DEALAY_TIME : float = 0.5
+
+
+func _init() -> void:
+	init()
+
+
+func _ready() -> void:
+	ready()
+	yield(get_tree().create_timer(LOAD_DEALAY_TIME), "timeout")
+	Events.emit_signal("level_loaded")
+
+
+func init() -> void:
+	pass
+
+
+func ready() -> void:
+	pass
