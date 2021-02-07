b54ae9df9abd59d3a9d40eb708eea2afc87e68ff
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-02-07 22:47:01 +0300

    Create and add main menu base node

--- singletons/LevelController.gd
+++ singletons/LevelController.gd
@@ -1,14 +1,16 @@
 extends Node
 
-const MAIN_MENU_PATH : String = "res://levels/Menus/MainMenu.tscn"
 const LEVELS : Array = [
 	"res://levels/splash/SplashScreenGodot.tscn",
 	"res://levels/splash/SplashScreenJam.tscn",
-	"res://levels/splash/SplashScreenTeam.tscn"
+	"res://levels/splash/SplashScreenTeam.tscn",
+	"res://levels/menus/MainMenu/MainMenu.tscn"
 ]
+const MAIN_MENU_ID : int = 3
 
 var current_level : BaseLevel
 var current_level_id : int = 0
+
 
 func load_level_by_id(id: int) -> void:
 	if id < 0 or id >= LEVELS.size():
@@ -19,7 +21,7 @@ func load_level_by_id(id: int) -> void:
 
 
 func load_main_menu() -> void:
-	# get_tree().change_scene(MAIN_MENU_PATH)
+	load_level_by_id(MAIN_MENU_ID)
 	pass
 
 
@@ -30,12 +32,7 @@ func load_next_level() -> void:
 		print_debug("last level")
 		load_main_menu()
 		return;
-	# if current_level.level_id > -1:
-	# 	var new_level_id = current_level.level_id + 1
-		
-	# 	if new_level_id >= LEVELS.size():
-	# 		load_main_menu()
-	# 		return
-
+	
 	get_tree().change_scene(LEVELS[new_level_id])
+	current_level_id += 1
 	pass
