16b0ce6c810f5eba0182c029432aac704af7bcf7
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-02-19 00:58:42 +0300

    Slight update for BaseTrigger and LifespanTimer rework

--- src/LifespanTimer/LifespanTimer.gd
+++ src/LifespanTimer/LifespanTimer.gd
@@ -5,22 +5,13 @@ extends Timer
 
 onready var parent = get_parent()
 
-var is_active : bool = true
 
-
-func _init() -> void:
+func _init(duration: float) -> void:
 	one_shot = true
-	autostart = true
+	wait_time = duration
 
 
 func _ready() -> void:
-	if not is_active:
-		return
-	
 	yield(get_parent(), "ready")
-	connect("timeout", parent, "queue_free")
-
-
-func _set_is_active(value: bool) -> void:
-	is_active = value
-	stop()
+	connect("timeout", parent, "queue_free", [CONNECT_ONESHOT])
+	start()
