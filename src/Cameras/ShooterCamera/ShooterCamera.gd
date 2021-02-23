61aff6904e2e11848761ea1874975f6c6d55fd65
Tricky Fat Cat <tricky.fat.cat@gmail.com>
2021-02-23 14:39:23 +0300

    Add new ShooterCamera class

--- src/Cameras/ShooterCamera/ShooterCamera.gd
+++ src/Cameras/ShooterCamera/ShooterCamera.gd
@@ -1,24 +1,13 @@
-class_name CameraController
-extends Position2D
+class_name ShooterCamera
+extends BaseCamera
 
 const CAMERA_FOLLOW_SPEED : float = 0.1
-
-onready var camera: Camera2D = $PlayerCamera
-onready var cameraShaker := $PlayerCamera/CameraShaker
 
 export var offset: = Vector2(80.0, 80.0)
 export var camera_range: = Vector2(0.0, 200.0)
 
 
-func _init() -> void:
-	Events.connect("shake_camera", self, "apply_camera_shake")
-
-
-func _physics_process(delta: float) -> void:
-	_update_position()
-
-
-func _update_position(velocity: Vector2 = Vector2.ZERO) -> void:
+func _update_position() -> void:
 	var distance_ratio : float
 	var target_position : Vector2
 
@@ -36,12 +25,3 @@ func _update_position(velocity: Vector2 = Vector2.ZERO) -> void:
 			pass
 		
 	camera.position = lerp(camera.position, target_position, CAMERA_FOLLOW_SPEED)
-
-
-func apply_camera_shake(
-	duration: float = 0.2,
-	frequency: float = 15.0,
-	amplitude: int = 16,
-	priority: int = 0
-	) -> void:
-	cameraShaker.start(duration, frequency, amplitude, priority)
